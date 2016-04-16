55849b76e87338d7467397c7795ebff78ac67592
JackQinCh <jackqin3956@gmail.com>
2016-04-16 03:36:58 -0400

    Add Local Database Configuration

--- config.php
+++ config.php
@@ -5,7 +5,12 @@
  * Date: 4/14/16
  * Time: 11:45
  */
-define('DB_SERVER', 'sql2.njit.edu');
-define('DB_USERNAME', 'zq42');
-define('DB_PASSWORD', '94m9dd71');
-define('DB_DATABASE', 'zq42');
+//define('DB_SERVER', 'sql2.njit.edu');
+//define('DB_USERNAME', 'zq42');
+//define('DB_PASSWORD', '94m9dd71');
+//define('DB_DATABASE', 'zq42');
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '0000');
+define('DB_DATABASE', 'Library');
