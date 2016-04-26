0c858f4a2ba1974e02f2b2058997aa70610fcf43
JackQinCh <jackqin3956@gmail.com>
2016-04-26 17:16:51 -0400

    Add Compute Fine SQL Function

--- reader_borrowed.php
+++ reader_borrowed.php
@@ -18,25 +18,18 @@ include('layout/reader_sidebar.php');
         die("Connection failed: " . $conn->connect_error);
     }
     //
-    $sql = "SELECT D.DOCID, D.TITLE, B.BDTIME, B.BORNUMBER
+    $sql = "SELECT D.DOCID, D.TITLE, B.BDTIME, B.BORNUMBER, COMPUTE_REMAIN_DAYS(B.BORNUMBER) AS DAYS, COMPUTE_FINE(B.BORNUMBER) AS FINE
             FROM DOCUMENT D, BORROWS B
-            WHERE D.DOCID = B.DOCID AND B.READERID = '$readerId' AND B.RDTIME IS NULL";
+            WHERE D.DOCID = B.DOCID AND B.READERID = 'jz01' AND B.RDTIME IS NULL;";
 
     $result = $conn->query($sql);
     if ($result->num_rows > 0){
         while ($row = $result->fetch_assoc()){
-            date_default_timezone_set('America/New_York');
-            $now = new DateTime();
-            $bortime = new DateTime($row['BDTIME']);
-            $interval = $now->diff($bortime);
-            $fineLabel = '';
-            $day = $interval->d - 20;
-            if ($day > 0){
-                $fine = $day * 0.2;
-                $fineLabel = "<span class='label label-danger searchlist-label'>fine $$fine</span>";
+
+            if ($row['DAYS'] == 0){
+                $fineLabel = "<span class='label label-danger searchlist-label'>fine $".$row['FINE']."</span>";
             }else{
-                $day = -$day;
-                $fineLabel = "<span class='label label-success searchlist-label'>$day days remained</span>";
+                $fineLabel = "<span class='label label-success searchlist-label'>".$row['DAYS']." days remained</span>";
             }
 
             echo "<div class='card card-block search-list-item'>
@@ -61,17 +54,18 @@ include('layout/reader_sidebar.php');
         die("Connection failed: " . $conn->connect_error);
     }
     //
-    $sql = "SELECT D.DOCID, D.TITLE, B.BDTIME, B.BORNUMBER, B.RDTIME
+    $sql = "SELECT D.DOCID, D.TITLE, B.BDTIME, B.BORNUMBER, B.RDTIME, COMPUTE_FINE(B.BORNUMBER) AS FINE
             FROM DOCUMENT D, BORROWS B
-            WHERE D.DOCID = B.DOCID AND B.READERID = '$readerId' AND B.RDTIME IS NOT NULL";
+            WHERE D.DOCID = B.DOCID AND B.READERID = 'jz01' AND B.RDTIME IS NOT NULL;";
 
     $result = $conn->query($sql);
     if ($result->num_rows > 0){
         while ($row = $result->fetch_assoc()){
 
             echo "<div class='card card-block search-list-item'>
-                    <h4 class='card-title'>".$row['DOCID'].": ".$row['TITLE'].
-                "</h4><p class='card-text text-muted'>borrowed at ".$row['BDTIME']." &bull; returned at ".$row['RDTIME']."</p>
+                    <h4 class='card-title'>".$row['DOCID'].": ".$row['TITLE']. "
+                    <span class='label label-default searchlist-label'>fine $".$row['FINE']."</span></h4>
+                    <p class='card-text text-muted'>borrowed at ".$row['BDTIME']." &bull; returned at ".$row['RDTIME']."</p>
                 </div>";
         }
     }else{
