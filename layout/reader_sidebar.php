d9169d28eaeabb005b3a616444cd6dd87a2f7a18
JackQinCh <jackqin3956@gmail.com>
2016-05-01 11:27:20 -0400

    Add Reserved and Borrowed Indicators at Sidebar

--- layout/reader_sidebar.php
+++ layout/reader_sidebar.php
@@ -1,4 +1,28 @@
 <!-- // Sidebar-->
+<?php
+// Create connection
+$conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//
+$sql = "SELECT COUNT(RESUMBER)
+            FROM RESERVES
+            WHERE READERID = '$readerId'
+            GROUP BY READERID";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$num_reserved = $row['COUNT(RESUMBER)'];
+//
+$sql = "SELECT COUNT(BORNUMBER)
+            FROM BORROWS
+            WHERE READERID = '$readerId' AND RDTIME IS NULL
+            GROUP BY READERID";
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$num_borrowed = $row['COUNT(BORNUMBER)'];
+?>
 <div class="sidebar">
     <div class="sidebar-heading">Roles</div>
     <ul class="sidebar-menu">
@@ -26,12 +50,16 @@
         <li class="sidebar-menu-item <?php echo ($page == 'Reserved') ? "active" : ""; ?>">
             <a href="reader_reserves.php" class="sidebar-menu-button">
                 <i class="sidebar-menu-icon material-icons">lock</i>
-                Reserved</a>
+                Reserved
+                <span class="sidebar-menu-label label label-default"><?php echo $num_reserved?></span>
+            </a>
         </li>
         <li class="sidebar-menu-item <?php echo ($page == 'Borrowed') ? "active" : ""; ?>">
             <a href="reader_borrowed.php" class="sidebar-menu-button">
                 <i class="sidebar-menu-icon material-icons">check_circle</i>
-                Borrowed</a>
+                Borrowed
+                <span class="sidebar-menu-label label label-default"><?php echo $num_borrowed?></span>
+            </a>
         </li>
         <li class="sidebar-menu-item <?php echo ($page == 'Reservations') ? "active" : ""; ?>">
             <a href="reader_findreader.php" class="sidebar-menu-button">
