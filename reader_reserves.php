ab17860a99dadf069a5aaa330f33e6f0d0c42755
JackQinCh <jackqin3956@gmail.com>
2016-05-01 10:44:51 -0400

    Borrow Limit 10 Disabled Check Out Button

--- reader_reserves.php
+++ reader_reserves.php
@@ -7,7 +7,6 @@ include('layout/reader_sidebar.php');
 
 <!-- // Content -->
 <div class="container layout-content">
-    <h4 class="text-muted">Reserved List</h4>
     <!-- Reserves List -->
     <?php
     // Create connection
@@ -15,6 +14,18 @@ include('layout/reader_sidebar.php');
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT COUNT(BORNUMBER)
+            FROM BORROWS
+            WHERE READERID = '$readerId' AND RDTIME IS NULL
+            GROUP BY READERID";
+    $result = $conn->query($sql);
+    $row = $result->fetch_assoc();
+    $borrow_num = $row['COUNT(BORNUMBER)'];
+    if ($borrow_num >= 10){
+        $checkout_class = 'disabled';
+    }else{
+        $checkout_class = '';
     }
     //
     $sql = "SELECT D.DOCID, D.TITLE, R.DTIME, C.POSITION, B.LNAME, B.LLOCATION, R.RESUMBER
@@ -29,12 +40,12 @@ include('layout/reader_sidebar.php');
                     <h4 class='card-title'>".$row['DOCID'].": ".$row['TITLE'].
                 "</h4><p class='card-text text-muted'>reserved at ".$row['DTIME']."</p>
                 <p class='card-text'>Position: ".$row['LNAME']." (".$row['LLOCATION'].") &bull; ".$row['POSITION']."</p>
-                <a class='btn btn-success btn-sm' href='checkout_reserve.php?resid=".$row['RESUMBER']."'>Check Out</a>
+                <a class='btn btn-success btn-sm $checkout_class' href='checkout_reserve.php?resid=".$row['RESUMBER']."'>Check Out</a>
                 <a class='btn btn-danger btn-sm' href='cancel_reserve.php?resid=".$row['RESUMBER']."'>Cancel</a>
                 </div>";
         }
     }else{
-        echo "0 reserved";
+        echo "<p>0 reserved</p>";
     }
     $conn->close();
     ?>
